dd9337d9248b4027c565cebf724c01ecb042c11e
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2021-01-17 13:19:02 +0530

    added init function

--- teddy.py
+++ teddy.py
@@ -4,9 +4,12 @@ import click
 import os, json
 from markdown_to_json.vendor import CommonMark
 from markdown_to_json.markdown_to_json import Renderer, CMarkASTNester
+from git import Repo
 
 projects_path = "/home/satyendra/projects"
 project_config = open("PROJECTS.md","r")
+
+git_ssh_prefix = "git@github.com:SatyendraBanjare/"
 
 def extract_json(fileName):
     ast = CommonMark.DocParser().parse(fileName.read())
@@ -14,32 +17,35 @@ def extract_json(fileName):
     orderdDict = Renderer().stringify_dict(nestedAst)
     return json.loads(json.dumps(orderdDict,indent=4))
 
-json_str = extract_json(project_config)
-print(json_str)
 # WIP repos :
-wip_repos = json_str['WIP']
+wip_repos = extract_json(project_config)['WIP']
 dir_list = os.listdir(projects_path)
-print(wip_repos)
-print(dir_list)
-for repo in wip_repos:
-    if repo in dir_list:
-        filePath = os.path.join(projects_path, repo, "README.md")
-        fileName = open(filePath,"r")
-        print(repo)
-        print(extract_json(fileName))
 
-var1 = 0
+def count_todo():
+    count = 0
+    for repo in wip_repos:
+        if repo in dir_list:
+            filePath = os.path.join(projects_path, repo, "README.md")
+            fileName = open(filePath,"r")
+            count+=len(extract_json(fileName)['TODO'])
+    return count
+
+def intialize_wip_repos():
+    for repo in wip_repos:
+        if repo not in dir_list:
+            dirPath = os.path.join(projects_path, repo)
+            clone_url = git_ssh_prefix + repo
+            Repo.clone_from(clone_url,dirPath)
+        else:
+            print("Repository " + repo +" already exists.")
 
 @click.group()
-@click.option('--verbose/--no-verbose', default=False , help='print verbosely')
-def cli(verbose):
-    global var1 
-    var1 = 10
-    click.echo('Debug mode is %s' % ('on' if verbose else 'off'))
+def cli():
+    pass
 
-@cli.command('initialize', short_help='initize with the repos in PROJECTS.md')  
-def intialize():
-    click.echo(var1)
+@cli.command('intirepos', short_help='initialize with the WIP repos in PROJECTS.md')  
+def intirepos():
+    click.echo(intialize_wip_repos())
 
 @cli.command('sync', short_help='sync the NOTES repo')  
 def sync():
@@ -47,7 +53,7 @@ def sync():
 
 @cli.command('todo', short_help='count To-Do items in WIP projects')  
 def todo():
-    click.echo('todo')
+    click.echo(count_todo())
 
 if __name__ == '__main__':
     cli()
